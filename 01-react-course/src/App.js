77b0e20ea8f6f50a6e140d0c427a531c15e1307f
Maximiliano Cóppola <maxicopp@outlook.com>
2023-06-02 19:24:51 -0300

    Creating "wrapper" components

--- 01-react-course/src/App.js
+++ 01-react-course/src/App.js
@@ -3,12 +3,11 @@ import { Route, Switch } from 'react-router-dom';
 import AllMetupsPage from './pages/AllMetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Switch>
         <Route path="/" exact>
           <AllMetupsPage />
@@ -20,7 +19,7 @@ function App() {
           <FavoritesPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 

1b60f004378a344a6173ee12b1953b206f4072ea
Maximiliano Cóppola <maxicopp@outlook.com>
2023-06-08 14:35:51 -0300

    Extracting dynamic path segement data (dynamic routes)

--- 02-pages-file-based-routing/pages/portfolio/[projectid].js
+++ 02-pages-file-based-routing/pages/portfolio/[projectid].js
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/router';
+
 function PortfolioProjectPage() {
+  const router = useRouter();
+  const { projectid } = router.query;
+
+  console.log(router);
+
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
+      <p>Project ID: {projectid}</p>
     </div>
   );
 }

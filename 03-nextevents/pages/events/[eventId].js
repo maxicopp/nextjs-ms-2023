e5615474936f91f07d3e257db3bbe97f5ef861c1
maxicopp <maxicopp@outlook.com>
2023-06-12 01:46:58 -0300

    Adding the EventDetail page

--- 03-nextevents/pages/events/[eventId].js
+++ 03-nextevents/pages/events/[eventId].js
@@ -1,8 +1,29 @@
+import { getEventById } from '../../dummy-data';
+import { useRouter } from 'next/router';
+import EventLogistics from '../../components/event-detail/event-logistics';
+import EventSummary from '../../components/event-detail/event-summary';
+import EventContent from '../../components/event-detail/event-content';
+
 function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-    </div>
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>{event.description}</EventContent>
+    </>
   );
 }
 

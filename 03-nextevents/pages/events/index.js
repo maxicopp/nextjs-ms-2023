004b42c23bd13a4bf8896b195f4ac4b94bcb9cff
Maximiliano Cóppola <maxicopp@outlook.com>
2023-06-14 00:50:26 -0300

    Adding a filter form for filtering events

--- 03-nextevents/pages/events/index.js
+++ 03-nextevents/pages/events/index.js
@@ -1,10 +1,16 @@
-import EventList from '../../components/events/event-list';
 import { getAllEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
 
 function AllEventsPage() {
   const allEvents = getAllEvents();
 
-  return <EventList items={allEvents} />;
+  return (
+    <>
+      <EventsSearch />
+      <EventList items={allEvents} />
+    </>
+  );
 }
 
 export default AllEventsPage;

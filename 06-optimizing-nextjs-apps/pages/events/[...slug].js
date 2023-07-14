60b84dc7525942d51225df86e87f5a88b39d634c
Maximiliano Cóppola <maxicopp@outlook.com>
2023-07-13 23:49:17 -0300

    Adding dynamic "head" content

--- 06-optimizing-nextjs-apps/pages/events/[...slug].js
+++ 06-optimizing-nextjs-apps/pages/events/[...slug].js
@@ -1,8 +1,8 @@
 import { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import useSWR from 'swr';
 
-import { getFilteredEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
 import ResultsTitle from '../../components/events/results-title';
 import Button from '../../components/ui/button';
@@ -90,6 +90,13 @@ function FilteredEventsPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>Filtered Events</title>
+        <meta
+          name="description"
+          content={`All events for ${numMonth}/${numYear}.`}
+        />
+      </Head>
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </Fragment>

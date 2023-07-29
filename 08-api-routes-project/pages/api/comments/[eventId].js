750bb276e787a0f81e9935ef57dd83bf10807caf
Maximiliano Cóppola <maxicopp@outlook.com>
2023-07-29 13:19:48 -0300

    Adding comments API routes

---
+++ 08-api-routes-project/pages/api/comments/[eventId].js
@@ -0,0 +1,50 @@
+const { v4: uuidv4 } = require('uuid');
+
+function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  if (req.method === 'POST') {
+    const { email, name, text } = req.body;
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !text ||
+      text.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input.' });
+      return;
+    }
+
+    const newComment = {
+      id: uuidv4(),
+      email,
+      name,
+      text,
+    };
+
+    console.log(newComment);
+    res.status(201).json({ message: 'Added comment.', comment: newComment });
+  }
+
+  if (req.method === 'GET') {
+    const dummyList = [
+      {
+        id: 'c1',
+        name: 'Max',
+        text: 'A first comment!',
+      },
+      {
+        id: 'c2',
+        name: 'Manu',
+        text: 'A second comment!',
+      },
+    ];
+
+    res.status(200).json({ comments: dummyList });
+  }
+}
+
+export default handler;

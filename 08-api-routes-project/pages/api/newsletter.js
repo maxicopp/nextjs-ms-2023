e20cdb1d841fd0e390441a9d591a578e6cc7356b
Maximiliano Cóppola <maxicopp@outlook.com>
2023-07-31 08:00:18 -0300

    Adding error handling

--- 08-api-routes-project/pages/api/newsletter.js
+++ 08-api-routes-project/pages/api/newsletter.js
@@ -1,5 +1,17 @@
 import { MongoClient } from 'mongodb';
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(process.env.MONGODB_URL);
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  const collection = db.collection('newsletter');
+  const result = await collection.insertOne(document);
+  return result;
+}
+
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
@@ -9,22 +21,24 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(process.env.MONGODB_URL);
+    let client;
 
-    const db = client.db();
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to database failed.' });
+      return;
+    }
 
-    const emailsCollection = db.collection('newsletter');
+    try {
+      await insertDocument(client, { email: userEmail });
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting document failed.' });
+      return;
+    }
 
-    const result = await emailsCollection.insertOne({
-      email: userEmail,
-    });
-
-    console.log(result);
-
-    client.close();
-
-    console.log({ userEmail });
     res.status(201).json({ message: 'Signed up!' });
+    client.close();
   }
 }
 

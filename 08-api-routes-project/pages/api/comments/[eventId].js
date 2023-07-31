1f98d0dfe54fa72dee55c761062d6e146356a11c
Maximiliano Cóppola <maxicopp@outlook.com>
2023-07-31 06:03:50 -0300

    Running MongoDB queries from inside API routes

--- 08-api-routes-project/pages/api/comments/[eventId].js
+++ 08-api-routes-project/pages/api/comments/[eventId].js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from 'uuid';
 
 function handler(req, res) {
   const eventId = req.query.eventId;
--- 08-api-routes-project/pages/api/newsletter.js
+++ 08-api-routes-project/pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -6,6 +8,20 @@ function handler(req, res) {
       res.status(422).json({ message: 'Invalid email address.' });
       return;
     }
+
+    const client = await MongoClient.connect(process.env.MONGODB_URL);
+
+    const db = client.db();
+
+    const emailsCollection = db.collection('emails');
+
+    const result = await emailsCollection.insertOne({
+      email: userEmail,
+    });
+
+    console.log(result);
+
+    client.close();
 
     console.log({ userEmail });
     res.status(201).json({ message: 'Signed up!' });

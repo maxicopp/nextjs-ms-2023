2b45d066a40e7c97fd69e6e4690a6b8f7ec4a5d4
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-05 21:00:46 -0300

    Comments notifications

--- 08-api-routes-project/components/input/comments.js
+++ 08-api-routes-project/components/input/comments.js
@@ -1,14 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CommentList from './comment-list';
 import NewComment from './new-comment';
 import classes from './comments.module.css';
+import { NotificationContext } from '../../store/notification-context';
 
 function Comments(props) {
   const { eventId } = props;
+  const notificationCtx = useContext(NotificationContext);
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
 
   async function fetchComments() {
     const response = await fetch(`/api/comments/${eventId}`);
@@ -18,8 +21,10 @@ function Comments(props) {
 
   useEffect(() => {
     if (showComments) {
+      setIsFetchingComments(true);
       fetchComments().then((data) => {
         setComments(data.comments);
+        setIsFetchingComments(false);
       });
     }
   }, [showComments]);
@@ -29,16 +34,42 @@ function Comments(props) {
   }
 
   async function addCommentHandler(commentData) {
-    const response = await fetch(`/api/comments/${eventId}`, {
-      method: 'POST',
-      body: JSON.stringify(commentData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
+    notificationCtx.showNotification({
+      title: 'Sending...',
+      message: 'Sending comment...',
+      status: 'pending',
     });
 
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await fetch(`/api/comments/${eventId}`, {
+        method: 'POST',
+        body: JSON.stringify(commentData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Sending comment failed!');
+      }
+
+      const data = await response.json();
+      console.log(data);
+
+      notificationCtx.showNotification({
+        title: 'Success!',
+        message: 'Comment added successfully!',
+        status: 'success',
+      });
+
+      setComments((prevComments) => [data.comment, ...prevComments]);
+    } catch (error) {
+      notificationCtx.showNotification({
+        title: 'Error!',
+        message: 'Sending comment failed!',
+        status: 'error',
+      });
+    }
   }
 
   return (
@@ -47,7 +78,8 @@ function Comments(props) {
         {showComments ? 'Hide' : 'Show'} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments && !isFetchingComments && <CommentList items={comments} />}
+      {showComments && isFetchingComments && <p>Loading...</p>}
     </section>
   );
 }

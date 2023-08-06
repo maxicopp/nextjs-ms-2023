387528bcf3e34d1eaa0be2070d4883907097ad86
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-06 18:52:36 -0300

    Adding the Hero component

--- 09-full-blog/components/home-page/hero.js
+++ 09-full-blog/components/home-page/hero.js
@@ -1,8 +1,24 @@
+import Image from 'next/image';
+import classes from './hero.module.css';
+
 function Hero() {
   return (
-    <div>
-      <h1>Hero</h1>
-    </div>
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/max.jpg"
+          alt="An image showing Max"
+          width={300}
+          height={300}
+          priority
+        />
+      </div>
+      <h1>Hi, I'm Max</h1>
+      <p>
+        I blog about web development - especially frontend frameworks like
+        Angular or React.
+      </p>
+    </section>
   );
 }
 

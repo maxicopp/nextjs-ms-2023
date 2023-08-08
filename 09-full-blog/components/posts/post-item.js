a3a61a575a9193451d6d0db2b74ca61b745ecbc3
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-07 23:45:27 -0300

    Adding a post grid & post items

--- 09-full-blog/components/posts/post-item.js
+++ 09-full-blog/components/posts/post-item.js
@@ -1,8 +1,37 @@
-function PostItem() {
+import Image from 'next/image';
+import Link from 'next/link';
+import classes from './post-item.module.css';
+
+function PostItem(props) {
+  const { title, image, excerpt, date, slug } = props.post;
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  });
+  const imagePath = `/images/posts/${slug}/${image}`;
+
   return (
-    <div>
-      <h1>Single Post Page</h1>
-    </div>
+    <li className={classes.post}>
+      <Link href="/posts/first-post">
+        <a>
+          <div className={classes.image}>
+            <Image
+              src={imagePath}
+              alt={title}
+              width={300}
+              height={200}
+              priority
+            />
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
   );
 }
 

38b9f4f80596ecc0f19938c3be110f112e828483
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-08 19:39:23 -0300

    Rendering dummy post data

--- 09-full-blog/components/posts/post-item.js
+++ 09-full-blog/components/posts/post-item.js
@@ -13,23 +13,22 @@ function PostItem(props) {
 
   return (
     <li className={classes.post}>
-      <Link href="/posts/first-post">
-        <a>
-          <div className={classes.image}>
-            <Image
-              src={imagePath}
-              alt={title}
-              width={300}
-              height={200}
-              priority
-            />
-          </div>
-          <div className={classes.content}>
-            <h3>{title}</h3>
-            <time>{formattedDate}</time>
-            <p>{excerpt}</p>
-          </div>
-        </a>
+      <Link href={`/posts/${slug}`}>
+        <div className={classes.image}>
+          <Image
+            src={imagePath}
+            alt={title}
+            width={300}
+            height={200}
+            priority
+            layout="responsive"
+          />
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <time>{formattedDate}</time>
+          <p>{excerpt}</p>
+        </div>
       </Link>
     </li>
   );
--- 09-full-blog/pages/index.js
+++ 09-full-blog/pages/index.js
@@ -2,11 +2,42 @@ import { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/fetured-posts';
 
+const DUMMY_POSTS = [
+  {
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Next.js is a React framework for server-side rendering.',
+    date: '2020-01-01',
+    slug: 'getting-started-with-nextjs',
+  },
+  {
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Next.js is a React framework for server-side rendering.',
+    date: '2020-01-01',
+    slug: 'getting-started-with-nextjs2',
+  },
+  {
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Next.js is a React framework for server-side rendering.',
+    date: '2020-01-01',
+    slug: 'getting-started-with-nextjs3',
+  },
+  {
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Next.js is a React framework for server-side rendering.',
+    date: '2020-01-01',
+    slug: 'getting-started-with-nextjs4',
+  },
+];
+
 function HomePage() {
   return (
     <Fragment>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }

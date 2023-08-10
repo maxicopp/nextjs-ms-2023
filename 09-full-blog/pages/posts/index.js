750a8c86e7305b01afa65c9a68a96302a1e58c6d
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-10 03:55:09 -0300

    Adding the "All posts" page

--- 09-full-blog/pages/posts/index.js
+++ 09-full-blog/pages/posts/index.js
@@ -1,9 +1,38 @@
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_POSTS = [
+  {
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Next.js is a React framework for server-side rendering.',
+    date: '2020-01-01',
+    slug: 'getting-started-with-nextjs',
+  },
+  {
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Next.js is a React framework for server-side rendering.',
+    date: '2020-01-01',
+    slug: 'getting-started-with-nextjs2',
+  },
+  {
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Next.js is a React framework for server-side rendering.',
+    date: '2020-01-01',
+    slug: 'getting-started-with-nextjs3',
+  },
+  {
+    title: 'Getting Started with Next.js',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'Next.js is a React framework for server-side rendering.',
+    date: '2020-01-01',
+    slug: 'getting-started-with-nextjs4',
+  },
+];
+
 function AllPostsPage() {
-  return (
-    <div>
-      <h1>All Posts</h1>
-    </div>
-  );
+  return <AllPosts posts={DUMMY_POSTS} />;
 }
 
 export default AllPostsPage;

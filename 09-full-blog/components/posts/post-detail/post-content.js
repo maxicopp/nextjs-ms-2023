7157a0b6ef0e7bb6c65f1026b096c0519588bc46
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-20 20:48:24 -0300

    Rendering code snippets from markdown

--- 09-full-blog/components/posts/post-detail/post-content.js
+++ 09-full-blog/components/posts/post-detail/post-content.js
@@ -2,6 +2,8 @@ import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 function PostContent({ post }) {
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
@@ -14,6 +16,22 @@ function PostContent({ post }) {
           width={600}
           height={300}
         />
+      );
+    },
+    code: ({ node, inline, className, children, ...props }) => {
+      const match = /language-(\w+)/.exec(className || '');
+      return !inline && match ? (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={match[1]}
+          PreTag="div"
+          children={String(children).replace(/\n$/, '')}
+          {...props}
+        />
+      ) : (
+        <code className={className} {...props}>
+          {children}
+        </code>
       );
     },
   };

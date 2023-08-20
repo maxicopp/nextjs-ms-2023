43fcd350747c1bdb9e8be934aae809429bd35b37
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-20 20:15:21 -0300

    Rendering images from markdown

--- 09-full-blog/components/posts/post-detail/post-content.js
+++ 09-full-blog/components/posts/post-detail/post-content.js
@@ -1,15 +1,27 @@
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 
 function PostContent({ post }) {
-  console.log(post);
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const customRenderers = {
+    img: (image) => {
+      return (
+        <Image
+          src={`/images/posts/${post.slug}/${image.src}`}
+          alt={image.alt}
+          width={600}
+          height={300}
+        />
+      );
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 }

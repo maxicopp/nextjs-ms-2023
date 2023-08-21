0e52412c5e26f182751297625a58ccb9224d5299
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-20 23:10:19 -0300

    Storing messages with mongodb in a database

--- 09-full-blog/pages/api/contact.js
+++ 09-full-blog/pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -13,14 +15,36 @@ function handler(req, res) {
       return res.status(422).json({ message: 'Invalid input.' });
     }
 
-    // TODO: store it in a database.
     const newMessage = {
       email,
       name,
       message,
     };
 
-    console.log(newMessage);
+    let client;
+
+    try {
+      client = await MongoClient.connect(process.env.MONGODB_URI);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'Could not connect to database.' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      console.log(error);
+      client.close();
+      res.status(500).json({ message: 'Storing message failed!' });
+      return;
+    }
+
+    client.close();
+
     return res
       .status(201)
       .json({ message: 'Successfully stored message!', message: newMessage });

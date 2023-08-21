b8b37a48d2a214b857a5b6e4b3f12e0022958fcc
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-20 21:42:41 -0300

    Adding the contact api route

---
+++ 09-full-blog/pages/api/contact.js
@@ -0,0 +1,30 @@
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !message ||
+      message.trim() === ''
+    ) {
+      return res.status(422).json({ message: 'Invalid input.' });
+    }
+
+    // TODO: store it in a database.
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    console.log(newMessage);
+    return res
+      .status(201)
+      .json({ message: 'Successfully stored message!', message: newMessage });
+  }
+}
+
+export default handler;

88efccf162da76072c9254b39b17eb6c93457089
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-20 23:55:14 -0300

    Adding "head" data

--- 09-full-blog/pages/index.js
+++ 09-full-blog/pages/index.js
@@ -2,10 +2,18 @@ import { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/fetured-posts';
 import { getFeaturedPosts } from '../lib/posts-util';
+import Head from 'next/head';
 
 function HomePage({ posts }) {
   return (
     <Fragment>
+      <Head>
+        <title>Max' Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={posts} />
     </Fragment>
--- 09-full-blog/pages/posts/[slug].js
+++ 09-full-blog/pages/posts/[slug].js
@@ -1,8 +1,18 @@
+import { Fragment } from 'react';
 import PostContent from '../../components/posts/post-detail/post-content';
-import { getAllPosts, getPostData, getPostsFiles } from '../../lib/posts-util';
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
+import Head from 'next/head';
 
 function PostDetailPage({ post }) {
-  return <PostContent post={post} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="description" content={post.excerpt} />
+      </Head>
+      <PostContent post={post} />
+    </Fragment>
+  );
 }
 
 export function getStaticProps(context) {

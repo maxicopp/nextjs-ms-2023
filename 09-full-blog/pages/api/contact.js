9a6166c64b5043d08aa9b02bf188c31651c3659a
Maximiliano Cóppola <maxicopp@outlook.com>
2023-08-27 19:47:55 -0300

    nextjs config file

--- 09-full-blog/pages/api/contact.js
+++ 09-full-blog/pages/api/contact.js
@@ -22,9 +22,10 @@ async function handler(req, res) {
     };
 
     let client;
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
 
     try {
-      client = await MongoClient.connect(process.env.MONGODB_URI);
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: 'Could not connect to database.' });

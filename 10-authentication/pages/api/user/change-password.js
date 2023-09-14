74f2720e9e05da74931ef2b84cb9c13c33468a53
Maximiliano Cóppola <maxicopp@outlook.com>
2023-09-14 01:56:26 -0300

    Sending a "change password" request from the frontend

--- 10-authentication/pages/api/user/change-password.js
+++ 10-authentication/pages/api/user/change-password.js
@@ -1,4 +1,6 @@
 import { getSession } from 'next-auth/client';
+import { ConnectToDatabase } from '../../../lib/db';
+import { hashPassword, verifyPassword } from '../../../lib/auth';
 
 async function handler(req, res) {
   if (req.method !== 'PATCH') {
@@ -10,6 +12,38 @@ async function handler(req, res) {
   if (!session) {
     return res.status(401).json({ message: 'Unauthorized' });
   }
+
+  const userEmail = session.user.email;
+  const oldPassword = req.body.oldPassword;
+  const newPassword = req.body.newPassword;
+
+  const client = await ConnectToDatabase();
+
+  const db = client.db();
+
+  const user = await db.collection('users').findOne({ email: userEmail });
+
+  if (!user) {
+    client.close();
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  const isValid = await verifyPassword(oldPassword, user.password);
+
+  if (!isValid) {
+    client.close();
+    return res.status(403).json({ message: 'Invalid password' });
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  await db
+    .collection('users')
+    .updateOne({ email: userEmail }, { $set: { password: hashedPassword } });
+
+  client.close();
+
+  return res.status(200).json({ message: 'Password changed successfully' });
 }
 
 export default handler;
